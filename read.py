d1ff0563b06c53f5ea9c3aca10563e6958cb4aab
Wayne Hung <thunder.wayne@gmail.com>
2019-06-18 16:45:12 +0800

    calc avg len reviews

--- read.py
+++ read.py
@@ -7,6 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-print(data[0])
-print('--------------------------------')
-print(data[1])
+print('檔案讀取完成，總共有 ', len(data), '筆資料。')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('每筆留言的平均長度為 ', sum_len/len(data))
