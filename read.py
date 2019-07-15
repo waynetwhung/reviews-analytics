3f85ceb4e74980dcdd46dcbe1e65f344010bbfd9
Wayne Hung <thunder.wayne@gmail.com>
2019-07-15 15:59:29 +0800

    add word count function

--- read.py
+++ read.py
@@ -28,3 +28,32 @@ for d in data:
 		good.append(d)
 print('一共有 ', len(good), '筆留言內容含有good')
 print(good[0])
+
+print(data[0])
+
+# 文字計數
+wc = {} # word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 # 新增新的key進wc字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('請問您想查詢什麼字： ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為： ', wc[word])
+	else:
+		print('查無此字！')
+
+print('感謝使用本查詢功能')
